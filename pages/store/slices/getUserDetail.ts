e597e91584db29091cf8108b47b91e4273a183f6
ESTEBAN QUINTERO <78872778+jesteban39@users.noreply.github.com>
2021-10-23 16:19:30 -0500

    store

---
+++ pages/store/slices/getUserDetail.ts
@@ -0,0 +1,49 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+
+import axios from "axios";
+
+import type { userDetail } from "types";
+
+export const getUserDetail = createAsyncThunk(
+  "users/getUserDetail",
+  async (userName: string) => {
+
+    const resUser: any = await axios.get(
+      `https://api.github.com/users/${userName}`
+    );
+    const resRepos: any = await axios.get(
+      `https://api.github.com/users/${userName}/repos`
+    );
+    const resOrgs: any = await axios.get(
+      `https://api.github.com/users/${userName}/orgs`
+    );
+    const user = {
+      id: resUser.data.id,
+      name: resUser.data.name,
+      login: resUser.data.login,
+      avatar_url: resUser.data.avatar_url,
+      repos: resRepos.data,
+      orgs: resOrgs.data,
+    };
+    console.log(user);
+    return user as userDetail;
+  }
+);
+
+export const userDetailSlice = createSlice({
+  name: "userDetail",
+  initialState: {
+    id: 0,
+    name: "",
+    login: "",
+    avatar_url: "",
+    repos: [],
+    orgs: [],
+  } as userDetail,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(getUserDetail.fulfilled, (state: userDetail, action) => {
+      return (state = action.payload);
+    });
+  },
+});
