243a9a68bb1260e7f87c3b61ea75aef846c41af4
ESTEBAN QUINTERO <78872778+jesteban39@users.noreply.github.com>
2021-10-23 17:02:25 -0500

    store

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,16 @@
-//import { Provider } from "react-redux";
+import { Provider } from "react-redux";
 
-//import store from "./store";
+import store from "../src/store";
 
 import type { AppProps } from "next/app";
 
 import "../styles/globals.css";
 
-{
-  /* <Provider store={store}></Provider> */
-}
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <Provider store={store}>
+      <Component {...pageProps} />
+    </Provider>
+  );
 }
 export default MyApp;
